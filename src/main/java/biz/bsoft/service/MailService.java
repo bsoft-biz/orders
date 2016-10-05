133d7fcee6af9aaa5ee1796c515ac530e6f2609e
Slava Babin <vbabin@bsoft.biz>
2016-10-05 15:59:58 +0300

    Less directory has been removed. MailService is tuned to denf new image. Added banner from opencms on top of application. Added counter.

--- src/main/java/biz/bsoft/service/MailService.java
+++ src/main/java/biz/bsoft/service/MailService.java
@@ -105,7 +105,7 @@ public class MailService {
             final String htmlContent = templateEngine.process(template, ctx);
             message.setText(htmlContent, true); // true = isHtml
             message.addInline("logo.png", new ClassPathResource("images/logo.png"), "image/png");
-            message.addInline("bsoft.biz.png", new ClassPathResource("images/poweredbybs.jpg"), "image/jpegloca");
+            message.addInline("poweredbybs.jpg", new ClassPathResource("images/poweredbybs.jpg"), "image/jpeg");
             mailSender.send(mimeMessage);
         }
         catch (MessagingException E){
---
+++ src/main/resources/static/js/banner.js
@@ -0,0 +1,17 @@
+/**
+ * Created by vbabin on 05.10.2016.
+ */
+function createIframe() {
+    var i = document.createElement("iframe");
+    i.src = "/baners/banner.jsp";
+    i.scrolling = "none";
+    i.frameborder = "0";
+    i.width = "100%";
+    i.height = "220px";
+    document.getElementById("bannerContainer").appendChild(i).style.border = "none";
+
+};
+
+if (window.addEventListener) window.addEventListener("load", createIframe, false);
+else if (window.attachEvent) window.attachEvent("onload", createIframe);
+else window.onload = createIframe;
