46e8d3c9e811e1ed6760364a9b727b3ee9d03db6
Slava Babin <vbabin@bsoft.biz>
2016-10-13 22:17:24 +0300

    Spring 1.4 allows us to use banner - so we added it, also it changed the way to test

--- src/test/java/biz/bsoft/OrdersApplicationTests.java
+++ src/test/java/biz/bsoft/OrdersApplicationTests.java
@@ -2,17 +2,21 @@ package biz.bsoft;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = OrdersApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrdersApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@Test
+	public void test() {
+		this.restTemplate.getForEntity(
+				"/users/user", String.class);
+	}
 }
