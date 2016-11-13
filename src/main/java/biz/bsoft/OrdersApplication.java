85ac3af6e1de9d14d59f94d42a9134624735b2b2
Slava Babin <vbabin@bsoft.biz>
2016-11-13 14:43:19 +0200

    Excess @Configuration annotation was removed.

--- src/main/java/biz/bsoft/OrdersApplication.java
+++ src/main/java/biz/bsoft/OrdersApplication.java
@@ -2,12 +2,8 @@ package biz.bsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class OrdersApplication{
 
 	public static void main(String[] args) {
