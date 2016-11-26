0310756294203dfea9b2d38a45b9bb3d601b0eb6
Slava Babin <vbabin@bsoft.biz>
2016-11-26 20:18:45 +0200

    Static files paths were changed.

--- src/main/java/biz/bsoft/config/SecurityConfig.java
+++ src/main/java/biz/bsoft/config/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.cors.CorsUtils;
 
 /**
  * Created by vbabin on 27.03.2016.
@@ -34,38 +33,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/users/**")
-//                .access("hasRole('ROLE_ADMIN')");
-
-        /*http.authorizeRequests()
-                .requestMatchers(CorsUtils::isCorsRequest).permitAll()
-                .anyRequest().authenticated();*/
-        //http.authorizeRequests().antMatchers("/users/**").authenticated().and().httpBasic();
         http
                 .httpBasic().and()
                 .authorizeRequests()
-                .antMatchers("/index.html", "/js/**", "/css/**", "/", "/users/resetPassword*", "/users/changePassword*").permitAll().anyRequest()
-                .authenticated()//.and().httpBasic()
+                .antMatchers("/index.html", "/js/**", "/locale/*", "/css/*", "/fonts/*", "/templates/*", "/", "/users/resetPassword*", "/users/changePassword*").permitAll().anyRequest()
+                .authenticated()
                 .and()
                 .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
-                .csrf().csrfTokenRepository(csrfTokenRepository());
-
-       /* http.authorizeRequests().antMatchers("/admin/**")
-                .access("hasRole('ROLE_ADMIN')").and().formLogin()
-                .loginPage("/login").failureUrl("/login?error")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .and().logout().logoutSuccessUrl("/login?logout")
-                .and().csrf()
-                .and().exceptionHandling().accessDeniedPage("/403");*/
+                .csrf().csrfTokenRepository(csrfTokenRepository())
+                .and()
+                .logout()
+                    .deleteCookies("JSESSIONID")
+                    .permitAll();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
+
     private CsrfTokenRepository csrfTokenRepository() {
         HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
         repository.setHeaderName("X-XSRF-TOKEN");
