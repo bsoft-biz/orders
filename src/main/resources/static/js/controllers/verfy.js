5c5ee26b6e68e824047c13278b5b17935841158f
Slava Babin <vbabin@bsoft.biz>
2016-12-11 22:17:04 +0200

    bug fixing in email verification process

--- src/main/resources/static/js/controllers/verfy.js
+++ src/main/resources/static/js/controllers/verfy.js
@@ -4,15 +4,16 @@ verify.$inject=['data','$http','$routeParams'];
 
 function verify(data,$http,$routeParams) {
     data.setTitle("TITLE_VERIFY");
+    var self=this;
     var token=$routeParams.token;
     if (token!==undefined)
     {
         $http.get("users/verify?token="+token).then(function(response) {
             console.log("verification succeeded");
-            $scope.result = "Ok";
+            self.result = "Ok";
         }, function(response) {
             console.log("verification failed");
-            $scope.result = "Sorry";
+            self.result = "Sorry";
         });
     }
 }
