03fd021c58a9b4e12ca18a4dc083b5aec074c563
Slava Babin <vbabin@bsoft.biz>
2016-12-12 22:28:49 +0200

    If token is empty then open home page.

--- src/main/resources/static/js/controllers/verfy.js
+++ src/main/resources/static/js/controllers/verfy.js
@@ -1,12 +1,15 @@
 angular.module('OrdersApp').controller('verify',verify);
 
-verify.$inject=['data','$http','$routeParams'];
+verify.$inject=['data','$http','$routeParams','$location','auth'];
 
-function verify(data,$http,$routeParams) {
+function verify(data,$http,$routeParams,$location,auth) {
     data.setTitle("TITLE_VERIFY");
     var self=this;
     var token=$routeParams.token;
-    if (token!==undefined)
+    if (token===undefined) {
+        $location.path(auth.homePath);
+    }
+    else
     {
         $http.get("users/verify?token="+token).then(function(response) {
             console.log("verification succeeded");
