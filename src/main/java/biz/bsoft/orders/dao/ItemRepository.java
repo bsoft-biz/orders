6b669fe4535df93a92671388516ea7e404e2bfc3
Slava Babin <vbabin@bsoft.biz>
2016-12-17 21:43:38 +0200

    Now we use archive property to filters archived items.

--- src/main/java/biz/bsoft/orders/dao/ItemRepository.java
+++ src/main/java/biz/bsoft/orders/dao/ItemRepository.java
@@ -1,11 +1,10 @@
 package biz.bsoft.orders.dao;
 
 import biz.bsoft.orders.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by vbabin on 25.09.2016.
- */
+import java.util.List;
+
 public interface ItemRepository extends CrudRepository<Item, Integer> {
+    List<Item> findByArchive(boolean archive);
 }
